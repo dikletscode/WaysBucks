d3b705a6144387e72852f349fbee2679f7f2994a
dikletscode <dikyrain0@gmail.com>
2021-08-21 16:31:23 +0700

    add dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import {
   BrowserRouter,
   BrowserRouter as Router,
@@ -7,11 +7,27 @@ import {
 } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/fullHeader";
-import Login from "./components/modal/auth/login";
-import Signup from "./components/modal/auth/signup";
 import HomePage from "./components/pages/home/HomePage";
+import { role } from "./components/types/roleEnum";
+import AuthContext from "./context/context";
 
 function App() {
+  const { state, dispatch } = useContext(AuthContext);
+
+  const loginUser = localStorage.getItem("_basicInfo");
+  useEffect(() => {
+    if (loginUser) {
+      const user = JSON.parse(loginUser);
+      if (role.SELLER == user.role) {
+        dispatch({ type: "ADMIN_LOGIN_SUCCESS", payload: user.detail.avatar });
+      } else if (role.BUYYER == user.role) {
+        dispatch({ type: "BUYYER_LOGIN_SUCCESS", payload: user.detail.avatar });
+      }
+    } else {
+      dispatch({ type: "LOGIN_FAILED", payload: null });
+    }
+  }, [loginUser]);
+
   return (
     <BrowserRouter>
       <Header />
