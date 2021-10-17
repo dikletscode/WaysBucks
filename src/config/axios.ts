9bffb92250fda29ab21fdee6400eaca04c9a0289
diky_herdian <dikyrain0@gmail.com>
2021-10-17 14:32:29 +0700

    url backend

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://waysback.herokuapp.com/api/v1/",
+  baseURL: "https://waysbuck.herokuapp.com/api/v1/",
 });
 
 API.interceptors.response.use(
